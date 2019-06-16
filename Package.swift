990a71050010a8e6e0000449b9ed13d54624ca89
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2019-06-15 17:04:46 -0700

    Add hello world code

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "icfpc2019",
+    products: [
+        .executable(name: "icfpc2019", targets: ["icfpc2019"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "icfpc2019", dependencies: [])
+    ]
+)
