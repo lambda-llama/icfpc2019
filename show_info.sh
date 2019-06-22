1e69fe23fc9c63f1c283e14ea5ee810de59a8ff4
Alexey Cherepanov <avcherepanov@gmail.com>
2019-06-22 17:07:10 +0100

    Delete unused stuff in show_info.sh

--- show_info.sh
+++ show_info.sh
@@ -2,9 +2,6 @@
 
 ./gradlew shadowJar
 
-OUTDIR=./solutions
-SOLUTION=$OUTDIR/solution-$(date --iso-8601=seconds).zip
-
 for f in $(find . -name '*.desc' | sort -n); do
     java -jar ./build/libs/icfpc2019.jar --non-interactive $f --info_only --show_bonus_count
 done
