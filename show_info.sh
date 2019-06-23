eb75fcd5e40e4d9335432f948c2c507d41438b1c
Alexey Cherepanov <avcherepanov@gmail.com>
2019-06-23 18:45:25 +0100

    Fix show_info.sh to filter irrelevant task descritiptions.

--- show_info.sh
+++ show_info.sh
@@ -2,6 +2,6 @@
 
 ./gradlew shadowJar
 
-for f in $(find . -name '*.desc' | sort -n); do
-    java -jar ./build/libs/icfpc2019.jar --non-interactive $f --info_only --show_bonus_count
+for f in $(find part-* -name '*.desc' | sort -n ); do
+    java -jar ./build/libs/icfpc2019.jar --non-interactive --info_only --show_bonus_count $f
 done
