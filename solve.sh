ce324ce940fa35d45b33e90953fb456d80db9a31
Alexey Cherepanov <avcherepanov@gmail.com>
2019-06-23 14:04:13 +0100

    Don't include irrelevant `sol` files to the archive

--- solve.sh
+++ solve.sh
@@ -10,4 +10,4 @@ SOLUTION=$OUTDIR/solution-$(date --iso-8601=seconds).zip
 find part-* -name '*.desc' | sort -n | parallel -P 4 --progress java -jar ./build/libs/icfpc2019.jar --non-interactive "$@"
 
 mkdir -p $OUTDIR
-find . -name '*.sol' -exec zip -j $SOLUTION {} \;
+find . -regextype sed -regex "./part.*/.*sol" -exec zip -j $SOLUTION {} \;
